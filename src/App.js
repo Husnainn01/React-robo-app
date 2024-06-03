081665c76c4fbeff21dacb51a4d042a6ab145b07
Husnainn01 <husnain@ss-japan.com>
2024-06-03 20:31:02 +0900

    Updated the Hooks and useState in the code

--- src/App.js
+++ src/App.js
@@ -1,43 +1,37 @@
-import React, { Component} from "react";
+import React, { useState, useEffect} from "react";
 import CardList from "./CardList"
 import SearchBox from "./SearchBox";
 import './App.css';
 import Scroll from './Scroll'
 
-class App extends Component {
-    constructor() {
-        super()
-        this.state = {
-        robots: [],
-        searchfield: ''
-        }
-    }
+function App() {
 
-    componentDidMount(){
+    const [robots, setRobots] = useState([])
+    const [searchfield, setSearchfield] = useState('')  // Corrected the typo here
+
+    useEffect (() => {
         fetch(`https://jsonplaceholder.typicode.com/users`)
-        .then(response=> response.json())
-        .then(users => this.setState({ robots: users }))
-    }
+            .then(response=> response.json())
+            .then(users => setRobots(users))
+    }, []);
 
 
-    onSearchChange = (event) => {
-        this.setState({ searchfield: event.target.value })
+    const onSearchChange = (event) => {
+        setSearchfield(event.target.value)
   
     }
-    render() {
-        const filteredRobots = this.state.robots.filter(robots =>{
-            return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
-        })
+    const filteredRobots = robots.filter(robot => {
+        return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+    })
         return (
             <div className='tc'>
                 <h1 className="f1">Robo Friends</h1>
-                <SearchBox searchChange = {this.onSearchChange}/>
+                <SearchBox searchChange = {onSearchChange}/>
                 <Scroll>
                 <CardList  robots = {filteredRobots}/>
                 </Scroll>
             </div>
         );
-    }
 }
 
 export default App;
